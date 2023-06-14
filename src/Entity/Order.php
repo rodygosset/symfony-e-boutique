fb974d171e24f681797b56526c383e94623b72e5
Rody Gosset <74539903+rodygosset@users.noreply.github.com>
2023-06-14 20:56:24 +0200

    psql test

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -9,7 +9,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 
-#[ORM\Table(name: '"order"')]
+#[ORM\Table(name: '`order`')]
 #[ORM\Entity(repositoryClass: OrderRepository::class)]
 class Order
 {
